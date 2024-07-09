ffd578b47691e76ebf138db173a06d5bfa4eb5ef
juniorlemos <47072463+juniorlemos@users.noreply.github.com>
2024-07-08 21:55:16 -0300

    Delete Coding.Chalenge.insfrastructure directory

--- Coding.Chalenge.insfrastructure/DataAccess/Interfaces/IContentWriteOnlyRepository.cs
+++
@@ -1,8 +0,0 @@
-﻿using Coding.Challenge.Dependencies.Models;
-
-namespace Coding.Challenge.insfrastructure.DataAccess.Interfaces
-{
-    public interface IContentWriteOnlyRepository : IGenericWriteOnlyRepository <Content>
-    {
-    }
-}
--- Coding.Chalenge.insfrastructure/DataAccess/Interfaces/IGenericWriteOnlyRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿
-using Coding.Challenge.Dependencies.Models;
-
-namespace Coding.Challenge.insfrastructure.DataAccess.Interfaces;
-
-public interface IGenericWriteOnlyRepository <T> where T : class
-{
-    Task<T?> CreateContent(T entity);
-}
